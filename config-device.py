12a18caa1f3c20eb92a35416ade72ef373279d62
renaud gaudin <reg@rskg.org>
2021-12-22 19:09:06 +0000

    restart container on env update

--- config-device.py
+++ config-device.py
@@ -147,6 +147,23 @@ def main(config_fpath):
     ps = subprocess.run(["/usr/bin/env", "systemctl", "restart", "dhcpcd5"])
     if ps.returncode != 0:
         return fatal(f"Failed to restart dhcpcd5: exited with {ps.returncode}")
+
+    if (
+        subprocess.run(
+            ["/usr/bin/env", "balena-engine", "inspect", "reverse-proxy"]
+        ).returncode
+        == 0
+    ):
+        subprocess.run(
+            [
+                "/usr/bin/env",
+                "docker-compose",
+                "-f",
+                "/root/docker-compose.yml",
+                "restart",
+                "reverse-proxy",
+            ]
+        )
     log("OK", GREEN)
     return 0
 
